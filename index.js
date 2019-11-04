ecc103e0180a5511b51207d4a21a49593f294b79
Kraoz <main.kraoz@gmail.com>
2019-11-04 11:55:33 +1100

    add jquery

--- index.js
+++
@@ -1,7 +0,0 @@
-import "./pdftron/webviewer.min.js";
-
-// import "./pdftron/ui-legacy/external/jquery-3.2.1.min.js";
-// import "./pdftron/CoreControls.js";
-// import "./pdftron/pdf/PDFNet.js";
-
-const toto = ({ tata, ...toto }) => console.log(tata, toto);
